82dc7cefb14618f304f1743cf702844b8f6832f9
byteyang <bytepine@gmail.com>
2025-07-02 15:12:31 +0800

    新建批处理模块

---
+++ Source/BatchProcessor/Private/BatchProcessor.cpp
@@ -0,0 +1,17 @@
+﻿#include "BatchProcessor.h"
+
+#define LOCTEXT_NAMESPACE "FBatchProcessorModule"
+
+void FBatchProcessorModule::StartupModule()
+{
+    
+}
+
+void FBatchProcessorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FBatchProcessorModule, BatchProcessor)
