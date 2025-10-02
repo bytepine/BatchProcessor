d8b891c6d1fdd73aab02de69506372bf04392f27
byteyang <bytepine@gmail.com>
2025-10-02 10:49:18 +0800

    添加useage文件

---
+++ Source/BatchProcessor/Private/Processor/UProcessor_Usage.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UProcessor_Usage.h"
+
+void UUProcessor_Usage::OnStart() const
+{
+	Super::OnStart();
+}
+
+bool UUProcessor_Usage::OnProcessing(void* Pointer, const UStruct* Struct) const
+{
+	return Super::OnProcessing(Pointer, Struct);
+}
+
+void UUProcessor_Usage::OnFinish() const
+{
+	Super::OnFinish();
+}
---
+++ Source/BatchProcessor/Private/Processor/UProcessor_Usage.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProcessorBase.h"
+#include "UProcessor_Usage.generated.h"
+
+/**
+ * 蓝图统计器
+ */
+UCLASS(DisplayName="蓝图统计器")
+class BATCHPROCESSOR_API UUProcessor_Usage : public UProcessorBase
+{
+	GENERATED_BODY()
+
+protected:
+	/**
+	 * 批处理开始
+	 */
+	virtual void OnStart() const override;
+
+	/**
+	 * 处理逻辑
+	 * @param Pointer 处理对象
+	 * @param Struct 处理结构
+	 * @return 是否有修改
+	 */
+	virtual bool OnProcessing(void* Pointer, const UStruct* Struct) const override;
+	
+	/**
+	 * 批处理结束
+	 */
+	virtual void OnFinish() const override;
+	
+	TSet<FString> BlueprintSet;
+};
