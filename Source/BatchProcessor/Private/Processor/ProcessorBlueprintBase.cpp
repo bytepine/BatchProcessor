9be8af099e263c4b0dcc5c5491a4916f366624eb
byteyang <bytepine@gmail.com>
2025-12-05 20:03:19 +0800

    新增蓝图处理器

---
+++ Source/BatchProcessor/Private/Processor/ProcessorBlueprintBase.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProcessorBlueprintBase.h"
+
+void UProcessorBlueprintBase::OnStart(UBatchContext* Context) const
+{
+	Super::OnStart(Context);
+	
+	OnStartBP(Context);
+}
+
+bool UProcessorBlueprintBase::OnProcessing(const UBlueprint* Assets, UBatchContext* Context,
+	const FBatchVariable& Variable) const
+{
+	bool bResult = Super::OnProcessing(Assets, Context, Variable);
+	
+	bResult |= OnProcessingBP(Assets, Context, Variable);
+	
+	return bResult;
+}
+
+void UProcessorBlueprintBase::OnFinish(UBatchContext* Context) const
+{
+	Super::OnFinish(Context);
+	
+	OnFinishBP(Context);
+}
+
+void UProcessorBlueprintBase::GetSubProcessors(TArray<UProcessorBase*>& SubProcessors) const
+{
+	Super::GetSubProcessors(SubProcessors);
+	
+	GetSubProcessorsBP(SubProcessors);
+}
---
+++ Source/BatchProcessor/Private/Processor/ProcessorBlueprintBase.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProcessorBase.h"
+#include "ProcessorBlueprintBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, Abstract, EditInlineNew)
+class BATCHPROCESSOR_API UProcessorBlueprintBase : public UProcessorBase
+{
+	GENERATED_BODY()
+	
+protected:
+	/**
+	 * @param Context 上下文
+	 * 批处理开始
+	 */
+	virtual void OnStart(UBatchContext* Context) const override;
+	
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName = "OnStart"))
+	void OnStartBP(UBatchContext* Context) const;
+	
+	/**
+	 * 处理逻辑
+	 * @param Assets 蓝图资产
+	 * @param Context 上下文
+	 * @param Variable 变量
+	 * @return 是否有修改
+	 */
+	virtual bool OnProcessing(const UBlueprint* Assets, UBatchContext* Context, const FBatchVariable& Variable) const override;
+	
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName = "OnProcessing"))
+	bool OnProcessingBP(const UBlueprint* Assets, UBatchContext* Context, const FBatchVariable& Variable) const;
+	
+	/**
+	 * @param Context 上下文
+	 * 批处理完成
+	 */
+	virtual void OnFinish(UBatchContext* Context) const override;
+	
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName = "OnFinish"))
+	void OnFinishBP(UBatchContext* Context) const;
+	
+	/**
+	 * 获取子处理器
+	 * @param SubProcessors 子处理器
+	 */
+	virtual void GetSubProcessors(TArray<UProcessorBase*>& SubProcessors) const override;
+	
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName = "GetSubProcessors"))
+	void GetSubProcessorsBP(TArray<UProcessorBase*>& SubProcessors) const;
+};
